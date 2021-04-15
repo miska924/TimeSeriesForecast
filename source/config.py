1dfaa3df8483b7a1ef8fbbd9d0f9fc9ddfe25bdc
miska924 <miska924code@gmail.com>
2021-04-15 15:11:40 +0300

    Fix for review

--- source/config.py
+++ source/config.py
@@ -1,7 +1,7 @@
 import enum
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 TICKERS = {
     'CBOM': ['PIKK', 'GAZP', 'PIKK'],
--- source/server/data_process.py
+++ source/server/data_process.py
@@ -10,20 +10,6 @@ from source._helpers import error_print
 from source import config as cfg
 
 
-def join(res, a, a_coef=1, b=None, b_coef=-1):
-    if a is not None:
-        a.name = "TMP"
-    if b is not None:
-        b.name = "TMP"
-
-    return res.join(
-        pd.DataFrame(index=res.index).join(
-            (a_coef * a if a is not None else 0) + (b_coef * b if b is not None else 0)
-        ).rename(columns={a.name: res.shape[1]}),
-        how='outer'
-    )
-
-
 class DataProcess:
 
     def __init__(self):
@@ -63,16 +49,32 @@ class DataProcess:
         result = result.dropna(axis=0, how='all')
         result.to_csv('table.csv')
 
+        prepared_dir = os.path.join(cfg.BASE_DIR, 'prepared')
         try:
-            os.stat('../../prepared')
+            os.stat(prepared_dir)
         except:
-            os.mkdir('../../prepared')
+            os.mkdir(prepared_dir)
+
 
         for TICK in result:
             prepared = DataProcess.get_prepared_data_frame(
                 pd.DataFrame(index=result.index).join(result[TICK]).join(result.copy().drop([TICK], axis=1))
             )
-            prepared.to_csv(f'../../prepared/{TICK}.csv')
+            prepared.to_csv(f'{prepared_dir}/{TICK}.csv')
+
+    @staticmethod
+    def _join(res, a, a_coef=1, b=None, b_coef=-1):
+        if a is not None:
+            a.name = "TMP"
+        if b is not None:
+            b.name = "TMP"
+
+        return res.join(
+            pd.DataFrame(index=res.index).join(
+                (a_coef * a if a is not None else 0) + (b_coef * b if b is not None else 0)
+            ).rename(columns={a.name: res.shape[1]}),
+            how='outer'
+        )
 
     @staticmethod
     def get_prepared_data_frame(df, diffs_count=2, x_lags=3, y_lags=4,
@@ -94,21 +96,21 @@ class DataProcess:
         average = res[0].copy()
         for k in range(res.shape[1], average_y_days):
             average += res[0].shift(k)
-        res = join(res, average, 1 / average_y_days)
+        res = DataProcess._join(res, average, 1 / average_y_days)
 
         # diffs:
         if diffs_count > 0:
-            res = join(res, res[0].copy(), 1, res[0].copy().shift(1), -1)
+            res = DataProcess._join(res, res[0].copy(), 1, res[0].copy().shift(1), -1)
         for i in range(res.shape[1], res.shape[1] + diffs_count - 1):
-            res = join(res, res[i - 1].copy(), 1, res[i - 1].copy().shift(1), -1)
+            res = DataProcess._join(res, res[i - 1].copy(), 1, res[i - 1].copy().shift(1), -1)
 
         # X lags:
         for i in range(1, res.shape[1]):
             for j in range(x_lags):
-                res = join(res, res[i].copy().shift(j + 1))
+                res = DataProcess._join(res, res[i].copy().shift(j + 1))
 
         # Y lags:
         for i in range(y_lags):
-            res = join(res, res[0].copy().shift(i + 1))
+            res = DataProcess._join(res, res[0].copy().shift(i + 1))
 
         return res
