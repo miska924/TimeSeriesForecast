e8853f6baf6083d4cadd849d12bba8bbf917cf49
Sergei Loptev <seloptev@gmail.com>
2021-04-15 17:39:13 +0300

    Change elements names and fill information in the interface
    Signed-off-by: Sergei Loptev <seloptev@gmail.com>

--- source/client/ui.py
+++ source/client/ui.py
@@ -4,12 +4,17 @@ import sys
 import source._helpers as hlp
 import source.client.config as ui_cfg
 import source.config as cfg
+<<<<<<< HEAD
 # TEST
 import plotly
 import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
 
+=======
+
+
+>>>>>>> 4d80474 (Change elements names and fill information in the interface)
 class GUI(QtWidgets.QMainWindow):
     def __init__(self):
         super(GUI, self).__init__()
@@ -93,6 +98,31 @@ class GUI(QtWidgets.QMainWindow):
         self.ui.webView.setHtml(html)
 
 
+<<<<<<< HEAD
+=======
+        self.ui.comboBox_series.addItem("")
+        self.ui.comboBox_model.addItem("")
+        self.ui.comboBox_metric.addItem("")
+        self.ui.comboBox_method.addItem("")
+        self.ui.comboBox_type.addItem("")
+        self.ui.comboBox_offset.addItem("")
+
+        self.ui.comboBox_series.addItems(cfg.TICKERS.keys())
+        self.ui.comboBox_model.addItems(hlp.get_values(cfg.Model))
+        self.ui.comboBox_metric.addItems(hlp.get_values(cfg.Metrics))
+        self.ui.comboBox_method.addItems(hlp.get_values(cfg.Methods))
+        self.ui.comboBox_type.addItems(hlp.get_values(cfg.Type))
+        self.ui.comboBox_offset.addItems(ui_cfg.TRANSLATE.keys())
+
+        self.ui.comboBox_series.currentTextChanged.connect(self.change_exogenous)
+
+    @QtCore.pyqtSlot(str)
+    def change_exogenous(self, ticker):
+        self.ui.listWidget.clear()
+        if (ticker):
+            self.ui.listWidget.addItems(cfg.TICKERS[ticker])
+
+>>>>>>> 4d80474 (Change elements names and fill information in the interface)
 app = QtWidgets.QApplication([])
 application = GUI()
 application.show()
