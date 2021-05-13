c68ba8d86a4e40212b23d40ef172579f8ec33d1f
Sergei Loptev <seloptev@gmail.com>
2021-05-13 16:51:10 +0400

    Attempt to use theme-defined colors for error handling
    Signed-off-by: Sergei Loptev <seloptev@gmail.com>

--- source/client/config.py
+++ source/client/config.py
@@ -8,14 +8,20 @@ TRANSLATE = {
     "Год": cfg.Offset.business_year
 }
 
-error_color = QtGui.QColor(255, 209, 220)
 tmp_app = QtWidgets.QApplication([])
 tmp = QtWidgets.QComboBox()
 correct_cb_color = tmp.palette().color(QtGui.QPalette.Button)
 tmp = QtWidgets.QLineEdit()
 correct_le_color = tmp.palette().color(QtGui.QPalette.Base)
+font_color = tmp.palette().color(QtGui.QPalette.WindowText)
 tmp = QtWidgets.QDateEdit()
 correct_de_color = tmp.palette().color(QtGui.QPalette.Base)
-print(error_color.getRgb(), correct_cb_color.getRgb(), correct_le_color.getRgb(), correct_de_color.getRgb(), sep='\n')
 del tmp_app
 del tmp
+
+if font_color.red() <= 127:
+    error_color = QtGui.QColor(255, 209, 220)
+else:
+    error_color = QtGui.QColor(143, 20, 57)
+
+print(error_color.getRgb(), correct_cb_color.getRgb(), correct_le_color.getRgb(), correct_de_color.getRgb(), font_color.getRgb(), sep='\n')
