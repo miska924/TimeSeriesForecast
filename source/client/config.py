f345e363e4b817b165de0da5560954f15cedb645
DanglSan <dangl11626@gmail.com>
2021-05-30 19:18:16 +0300

    Add no_trend for ets

--- source/client/config.py
+++ source/client/config.py
@@ -59,7 +59,8 @@ class TRANSLATE:
 
     ETSTrend = {
         "Аддитивный": cfg.ETSTrend.additive,
-        "Мультипликативный": cfg.ETSTrend.multiplicative
+        "Мультипликативный": cfg.ETSTrend.multiplicative,
+        "": cfg.ETSTrend.no_trend
     }
 
 tmp_app = QtWidgets.QApplication([])
